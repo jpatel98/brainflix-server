83dd3ff855bbcec4d0c19d201f020720712f2ac1
Jigar Patel <jigar@JP-machine.local>
2022-07-12 12:21:38 -0400

    Add a separate route for video id GET request

--- routes/videos.js
+++ routes/videos.js
@@ -17,21 +17,4 @@ router.get('/', (req, res) => {
     res.json(vidArrResponse);
   })
 
-  //GET request for '/videos/videoId'.
-// videoId will be swapped out with the id of a video as found in the list of videoDetails
-// Returns a detailed object of a single video
-// Details include the list of comments for that video
-router.get('/:videoId', (req, res) => {
-    const requestedVideoId = req.params.videoId;
-    const vidDetailsArr = vidArrParsed.videoDetails;
-    const requestedVideo = vidDetailsArr.find(video => video.id === requestedVideoId)
-  
-    if (!requestedVideo) {
-      res.status(404).send('Video not found');
-      return;
-    }
-  
-    res.status(200).json(requestedVideo);
-  })
-
   module.exports = router;

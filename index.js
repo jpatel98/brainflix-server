ddb8381735adcb5ce8ffa82adc44c1688d2403cc
Jigar Patel <jigar@JP-machine.local>
2022-07-13 18:53:53 -0400

    Add post method to '/videos' route

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const app = express();
 const videoRoutes = require('./routes/videos');
+const cors = require('cors');
+
+app.use(express.json());
+app.use(express.static('./public'));
+app.use(cors());
 
 
-app.use('/videos', videoRoutes);
-
-
+app.use('/', videoRoutes);
 
 app.listen(8080, () => {
     console.log(`Server fired up on port 8080 🫡`);
-  });
+});
--- routes/videos.js
+++ routes/videos.js
@@ -1,29 +1,42 @@
 const express = require("express");
 const router = express.Router();
 const fs = require('fs');
+const { v4: uuidv4 } = require('uuid');
 
-// videos.json file stored in a variable
-const videosJSONFile= fs.readFileSync('./data/videos.json');
-// parsing the buffer data after fs.readFileSync
-const vidArrParsed = JSON.parse(videosJSONFile);
+
+
+const readVideos = () => {
+  // videos.json file stored in a variable
+  // parsing the buffer data after fs.readFileSync
+  const videosJSONFile= fs.readFileSync('./data/videos.json');
+  const vidArrParsed = JSON.parse(videosJSONFile);
+  return vidArrParsed;
+}
 
 //GET request for '/videos'.
 // Returns an array of video objects
 // Contains only enough information to display in side bar
-router.get('/', (req, res) => {
+router.get('/videos', (req, res) => {
     //accessing videos object to send as response
-    const vidArrResponse = vidArrParsed.videos;
-    // sending the json data back as response
-    res.json(vidArrResponse);
+    const vidArr = readVideos();
+    const videosResponse = vidArr.map(vid => { 
+        return {
+          id: vid.id,
+          title: vid.title,
+          channel: vid.channel,
+          image: vid.image
+        }
+    })
+    res.json(videosResponse);
 })
 
 //GET request for '/videos/videoId'.
 // videoId will be swapped out with the id of a video as found in the list of videoDetails
 // Returns a detailed object of a single video
 // Details include the list of comments for that video
-router.get('/:videoId', (req, res) => {
+router.get('/videos/:videoId', (req, res) => {
     const requestedVideoId = req.params.videoId;
-    const vidDetailsArr = vidArrParsed.videoDetails;
+    const vidDetailsArr = readVideos();
     const requestedVideo = vidDetailsArr.find(video => video.id === requestedVideoId)
   
     if (!requestedVideo) {
@@ -34,4 +47,28 @@ router.get('/:videoId', (req, res) => {
     res.status(200).json(requestedVideo);
 })
 
-  module.exports = router;
+// POST request for /videos that will add a new video to the video list. 
+// A unique id must be generated for each video added.
+router.post('/videos', (req, res) => {
+  const { title, description } = req.body;
+  if(!title || !description) {
+    return res.status(400).send('Title and description are required fields');
+  }
+  
+  const vidArr = readVideos();
+
+  const newVideo = {
+    id: uuidv4(),
+    title,
+    description
+  };
+
+  vidArr.push(newVideo);
+
+  fs.writeFileSync('./data/videos.json', JSON.stringify(vidArr));
+  res.status(201).json(vidArr);
+
+})
+
+
+module.exports = router;
